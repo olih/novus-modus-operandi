6172e62f7182b00fcceb8444e528cd547f4a547d
Olivier Huin <olih@users.noreply.github.com>
2020-08-04 23:12:47 +0100

    Adds RegExpPersistence

--- python3/dsl_text.py
+++ python3/dsl_text.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple, Dict, Set, Optional
-
+import re
 
 def discard_empty(lines):
     return [line.strip() for line in lines if len(line.strip())>0]
@@ -67,4 +67,44 @@ class SequencePersistence(BasePersistence):
 
     def to_csv_string(self, values: List[str])->str:
          return "{} {} {}".format(self.cfg.start, self.cfg.separator.join(values), self.cfg.finish)
-        
+
+
+class RegExpConfig:
+    def __init__(self):
+        self.match = ".*"
+        self.pattern = re.compile(".*")
+        self.separator = " "
+
+    def set_match(self, match: str):
+        self.match = match
+        self.pattern = re.compile(match)
+        return self
+
+    def set_separator(self, separator: str):
+        self.separator = separator
+        return self
+
+    def match_str(self, chunk: str):
+        return self.pattern.match(chunk)
+       
+
+class RegExpPersistence(BasePersistence):
+    def __init__(self, cfg: RegExpConfig):
+        self.cfg = cfg
+   
+    def satisfy(self, strchunk: str)->bool:
+        trimmed = strchunk.strip()
+        finished = trimmed.find(self.cfg.separator)
+        candidate = trimmed if finished < 0 else trimmed[:finished]
+        return self.cfg.match_str(candidate) is not None
+
+    
+    def parse_as_string(self, strchunk: str)->(str, str):
+        pass
+
+    def parse_as_list(self, strchunk: str)->(List[str], str):
+        pass
+     
+    def to_csv_string(self, values: List[str])->str:
+        pass
+
