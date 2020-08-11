30d1e8d4e21c8b467c20a3732eaf64a451051936
Olivier Huin <olih@users.noreply.github.com>
2020-08-11 21:56:24 +0100

    Adds more spare dsl examples

--- python3/spare_dsl.py
+++ python3/spare_dsl.py
@@ -25,13 +25,43 @@ class ColorName(Enum):
         else:
             return "E"
 
+class SpareItem:
+    def __init__(self):
+        self.v_float: float = -1.00
+        self.v_fraction: Fraction = "1/1"
+
+    def set_v_float(self, value: float):
+            self.v_float = value
+            return self
+
+    def set_v_fraction(self, value: Fraction):
+            self.v_fraction = value
+            return self
+
+    def to_string(self):
+        return " ".join([
+            "v_float", str(self.v_float),
+            "v_fraction", str(self.v_fraction)
+        ])
+    
+    def __str__(self):
+        return self.to_string()
+    
+    def __repr__(self):
+        return self.to_string()
+
+    def __eq__(self, other):
+            return self.to_string() == str(other)
+
 class SpareRow:
-    def __init__(self, id: str):
-        self.id = id
-        self.v_int = -101
-        self.url = "http://define.me"
-        self.tags = []
-        self.color_name = ColorName.NOT_SUPPORTED
+    def __init__(self):
+        self.id:str = "no-id"
+        self.v_int: int = -101
+        self.url: str = "http://define.me"
+        self.tags: Set[str] = set([])
+        self.emails: List[str] = []
+        self.color_name: ColorName = ColorName.NOT_SUPPORTED
+        self.items: List[SpareItem] = []
 
     def set_v_int(self, value: int):
             self.v_int = value
@@ -41,14 +71,50 @@ class SpareRow:
             self.url = value
             return self
 
-    def set_tags(self, tags: List[str]):
+    def set_tags(self, tags: Set[str]):
             self.tags = tags
             return self
 
     def add_tag(self, value: str):
-        self.tags.append(value)
+        self.tags.add(value)
+        return self
+
+    def set_emails(self, emails: List[str]):
+            self.emails = emails
+            return self
+
+    def add_email(self, value: str):
+        self.emails.append(value)
         return self
 
     def set_color_name(self, value: ColorName):
         self.color_name = value
         return self
+
+    def set_items(self, items: List[SpareItem]):
+            self.items = items
+            return self
+
+    def add_item(self, value: SpareItem):
+        self.items.append(value)
+        return self
+
+    def to_string(self):
+        return " ".join([
+        "id", self.id,
+        "v_int", str(self.v_int),
+        "url", self.url,
+        "tags", str(self.tags),
+        "emails", str(self.emails),
+        "color_name", self.color_name.to_string(),
+        "items", str(self.items),
+        ])
+
+    def __str__(self):
+        return self.to_string()
+    
+    def __repr__(self):
+        return self.to_string()
+
+    def __eq__(self, other):
+            return self.to_string() == str(other)
