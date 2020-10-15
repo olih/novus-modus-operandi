fbb5967dc7211367d2fcb7c838a8c253937fb572
Olivier Huin <olih@users.noreply.github.com>
2020-10-15 08:46:10 +0100

    Adds model for item

--- python3/dsl_gen_helper.py
+++ python3/dsl_gen_helper.py
@@ -33,4 +33,29 @@ class GenEnumConfig(GenBase):
         return map(to_const_and_value, self.values)
 
 
+standard_types = ["str", "int", "float", "Fraction"]
+
+class ValueObj:
+    def __init__(self, name: str, value_type: str, default_value: str):
+        self.name = name
+        self.value_type = value_type
+        self.default_value = default_value
+        self.is_list = self.value_type.startswith("List[")
+        self.is_set = self.value_type.startswith("Set[")
+        self.core_value_type = value_type.replace("List[", "").replace("Set[", "").replace("]", "")
+        self.is_standard_type = self.core_value_type in standard_types
+     
+
+class GenValueObjConfig(GenBase):
+    def __init__(self):
+        self.name = "no-name"
+        self.values: List[ValueObj] = []
+
+    def set_name(self, name: str):
+        self.name = name
+        return self
+
+    def set_values(self, values: List[ValueObj]):
+        self.values = values
+        return self
     
